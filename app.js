bbe3bfbdc3e1517e2225c58f15d8cab90b0b2449
Chloe Alnaji <caedeal@gmail.com>
2015-11-13 10:27:49 -0700

    Added cors

--- app.js
+++ app.js
@@ -9,7 +9,11 @@ require('dotenv').load();
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
+var cors = require('cors');
+
 var app = express();
+
+app.use(cors());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
