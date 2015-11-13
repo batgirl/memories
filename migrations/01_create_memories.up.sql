c495f7f0cf653922462c6e0079de89ba749835c8
Chloe Alnaji <caedeal@gmail.com>
2015-11-12 20:51:25 -0700

    Fixed Swagger config

---
+++ migrations/01_create_memories.up.sql
@@ -0,0 +1,6 @@
+CREATE TABLE
+memories
+(id SERIAL PRIMARY KEY,
+old_days text,
+these_days text,
+year numeric);
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var sup = require('pg-supp');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.post('/api/v1/memories', function(req, res, next) {
+  sup.insert("memories", ["old_days", "these_days", "year"], ["Had to use a flip phone", "Tablet at age 2", "1994"]);
+  res.send("success");
+})
 
 module.exports = router;
